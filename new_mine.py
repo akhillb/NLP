676054f9963b633cffc09df61b4bf01b4a69dbfd
saathvik782 <saathvik782@gmail.com>
2014-11-29 03:09:45 +0530

    concept sequences done sufix tree to be bult

--- new_mine.py
+++ new_mine.py
@@ -21,51 +21,56 @@ output4 = open("url.txt","w")
 qi = 0
 ui = 0
 
-lines.remove(lines[0])
+line_no = 1
 for line in lines:
-    data = line.split()
-    if data[len(data)-1][0:4] == "http":
-	link = data[len(data)-1]
-	query = ""
-	for i in range(1,len(data)-4):
-	    query = query + data[i] + " "
-	query = query.strip(" ")
-	if not query_dict.has_key( query ):
-	    url = []
-	    url.append(link)
-	    query_dict[query] = url
-	    query_index[query] = qi
-	    print query + " " + str(qi)
-	    qj = qi
-	    qi = qi + 1
-	else:
-	    if link not in query_dict[query]:
-		query_dict[query].append( link )
-	    qj = query_index[query]
-	if not url_dict.has_key( link ):
-	    q = []
-	    q.append(query)
-	    url_dict[link] = q
-	    url_index[link] = ui
-	    print link + " " + str(ui)
-	    uj = ui
-	    ui = ui + 1
-	else:
-	    if link not in query_dict[query]:
-		url_dict[link].append( query )
-	    uj = url_index[link]
-	query_url = ( qj,uj )
-	if count.has_key( query_url ):
-	    count[query_url] = count[query_url] + 1
-	else:
-	    count[query_url] = 1
-
+    if line_no != 1:
+        data = line.split()
+        if data[len(data)-1][0:4] == "http":
+	   link = data[len(data)-1]
+    	   query = ""
+    	   for i in range(1,len(data)-4):
+    	       query = query + data[i] + " "
+    	   query = query.strip(" ")
+    	   if not query_dict.has_key( query ):
+    	       url = []
+    	       url.append(link)
+    	       query_dict[query] = url
+    	       query_index[query] = qi
+    	       print query + " " + str(qi)
+    	       qj = qi
+    	       qi = qi + 1
+    	   else:
+    	       if link not in query_dict[query]:
+    	   	query_dict[query].append( link )
+    	       qj = query_index[query]
+    	   if not url_dict.has_key( link ):
+    	       q = []
+    	       q.append(query)
+    	       url_dict[link] = q
+    	       url_index[link] = ui
+    	       print link + " " + str(ui)
+    	       uj = ui
+    	       ui = ui + 1
+    	   else:
+    	       if link not in query_dict[query]:
+    	   	url_dict[link].append( query )
+    	       uj = url_index[link]
+    	   query_url = ( qj,uj )
+    	   if count.has_key( query_url ):
+    	       count[query_url] = count[query_url] + 1
+    	   else:
+    	       count[query_url] = 1
+    line_no = line_no + 1
+    
 print "Writing query Mappings..."
 json.dump(query_dict,output1)
 output1.write("\n")
 json.dump(url_dict,output1)
+output1.write("\n")
+json.dump(query_index,output1)
 
 query_index = sorted(query_index.items(),key = lambda x:x[1])
+
 url_index = sorted(url_index.items(),key = lambda x:x[1])
 print "Writing Query indices..."
 for item in query_index:
