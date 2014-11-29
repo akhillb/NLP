fa4ce34b847462a282eef53eaaa89ea90eeaeb60
akhillb <akhil.lb1@gmail.com>
2014-11-29 09:41:46 +0530

    Search engine basic version done

--- Engine/search/src/Main.java
+++ Engine/search/src/Main.java
@@ -1,69 +1,36 @@
-import edu.cmu.lti.lexical_db.ILexicalDatabase;
-import edu.cmu.lti.lexical_db.NictWordNet;
-import edu.cmu.lti.ws4j.RelatednessCalculator;
-import edu.cmu.lti.ws4j.impl.WuPalmer;
-import engine.User;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import engine.User;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
 
 public class Main{
-	public static void main(String [] args) throws FileNotFoundException{
-		try{
-			String query;
-			boolean stop = false;
-			HashMap<Integer,String> queries = new HashMap<Integer,String>();
-			HashMap<Integer,String> links = new HashMap<Integer,String>();
-			HashMap<String,Integer> dictionary = new HashMap<String,Integer>();
-			HashMap<Integer,ArrayList<Integer>> maps = new HashMap<Integer,ArrayList<Integer>>();
-			System.out.println("Loading....");
-
-			FileInputStream fstream = new FileInputStream("/home/akhil/NLP/data/query.txt");
-			BufferedReader br1 = new BufferedReader(new InputStreamReader(fstream));
-			String strLine;
-			while( (strLine = br1.readLine()) != null ){
-				String [] data = strLine.split(" : ");
-				queries.put(Integer.parseInt(data[0]), data[1]);
-				dictionary.put(data[1],Integer.parseInt(data[0]));
+	public static void main(String[] args) throws Exception{
+		Socket sock = new Socket("127.0.0.1",3000);
+		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in)); 
+		// sending to client (pwrite object) 
+		OutputStream ostream = sock.getOutputStream(); 
+		PrintWriter pwrite = new PrintWriter(ostream, true);   
+		// receiving from server ( receiveRead object) 
+		InputStream istream = sock.getInputStream(); 
+		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+		System.out.println("Start the chitchat, type and press Enter key");  
+		String receiveMessage, sendMessage;
+		while(true) { 
+			sendMessage = keyRead.readLine(); 
+			// keyboard reading 
+			pwrite.println(sendMessage); 
+			// sending to server 
+			pwrite.flush(); 
+			// flush the data
+			if((receiveMessage = receiveRead.readLine()) != null) 
+				//receive from server
+				{ 
+					System.out.println(receiveMessage); 
+				// displaying at DOS prompt 
+				} 
+			else
+			{
+				System.out.println("Bye..\n");
+				sock.close();
 			}
-			
-			fstream = new FileInputStream("/home/akhil/NLP/data/url.txt");
-			br1 = new BufferedReader(new InputStreamReader(fstream));
-			while( (strLine = br1.readLine()) != null ){
-				String [] data = strLine.split(" : ");
-				links.put(Integer.parseInt(data[0]), data[1]);
-			}	
-			
-			fstream = new FileInputStream("/home/akhil/NLP/data/normalised.txt");
-			br1 = new BufferedReader(new InputStreamReader(fstream));
-			int i = 0,j=0;
-			while( (strLine = br1.readLine()) != null ){
-				String [] data = strLine.split(" ");
 			}
-
-			System.out.println("Enter your search queries");
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-			User user = new User(queries,dictionary,links);
-			while( !stop ){
-				System.out.print("> ");
-				if((query=br.readLine()) != null){
-					String results = user.getResults(query);
-					System.out.println(results);
-				}
-				else
-					stop = true;
-			}
-			System.out.println("\nBye...");
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
+		} 
 	}
-}
